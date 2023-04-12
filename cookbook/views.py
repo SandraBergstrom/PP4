2020e2821f08932f51ada4def66041b4b2a75e01
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-04-12 11:44:07 +0000

    Add base.html

--- cookbook/views.py
+++ cookbook/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import User
 
 def home(request):
     return render(request, 'home.html')
+
+def join(request):
+    return render(request, 'join.html')
