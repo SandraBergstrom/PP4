674db71ff63148a0547e33d1c22e702632b0b19e
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-04-12 13:20:38 +0000

    Connect join form to db and redirect to home

--- cookbook/views.py
+++ cookbook/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import User
+from .forms import UserForm
+from django.contrib import messages
 
 def home(request):
     return render(request, 'home.html')
 
 def join(request):
-    return render(request, 'join.html')
+    if request.method == 'POST':
+        form = UserForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        else: 
+            message.danger(request, ('There was an error filling out the form. Please try again!'))
+        messages.success(request, ('You are now a member!'))
+        return redirect('home')
+
+    else: 
+        return render(request, 'join.html')
