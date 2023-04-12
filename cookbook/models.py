7866dfe4a4c07cfdf6c100be0ae7b77b063f62ec
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-04-12 06:18:50 +0000

    Connect cloudinary

--- cookbook/models.py
+++ cookbook/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+# STATUS = ((0, "Private"), (1, "Public"))
+
+# class recipe(models.Model):
+#     title = models.CharField(max_length=200, unique=True)
+#     slug =  models.SlugField(max_length=200, unique=True)
+#     description = models.CharField(max_length=500)
+#     ingredients = models.
