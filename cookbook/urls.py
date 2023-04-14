875ef6a95e4e45c665a9d6192276ab3604f2d4cc
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-04-14 15:23:03 +0000

    Add recipe post template

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("", views.recipeList.as_view(), name="home"),
     path('join', views.join, name='join'),
+    path('<slug:slug>/', views.RecipePost.as_view(), name='recipe_post'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
--- cookbook/views.py
+++ cookbook/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse, redirect
-from django.views import generic
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic, View
 from .models import Member, Recipe 
 from .forms import MemberForm
 from django.contrib import messages
@@ -23,3 +23,24 @@ class recipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class RecipePost(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        query = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(query, slug=slug) # Fix here
+        comments = recipe.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipe_post.html",
+            {
+                'recipe': recipe,
+                'comments': comments,
+                'liked': liked
+            },
+        )
